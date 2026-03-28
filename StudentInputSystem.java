3e014b1c1093c6694cd18a3d66b1ef189be4b8a2
Payal <payallimje999999@gmail.com>
2026-03-28 22:44:14 +0530

    Program4

---
+++ StudentInputSystem.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class StudentInputSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter student age: ");
+            int age = sc.nextInt();
+
+            // Example ArithmeticException (optional logic)
+            int test = 10 / age;
+
+            System.out.println("Student age is: " + age);
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+        } catch (ArithmeticException e) {
+            System.out.println("Invalid input");
+        }
+    }
+}
